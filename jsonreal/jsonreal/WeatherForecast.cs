7b15c7704a7d0d531bc5e67a1cc7dcaa69c40978
Suotjhi <sgpha206@learner.dvusd.org>
2022-03-21 09:29:28 -0700

    k

---
+++ jsonreal/jsonreal/WeatherForecast.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace jsonreal
+{
+    class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
