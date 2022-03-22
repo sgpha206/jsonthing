ec6b0bda8e3e08b9f5c141b05391dd5d32d558d4
Suotjhi <sgpha206@learner.dvusd.org>
2022-03-22 09:29:00 -0700

    efw

---
+++ jsonreal/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
+
+namespace jsonreal
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            var phoenixForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2022-03-22"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            WriteLine(jsonString);
+
+            string phoenixstring = JsonSerializer.Serialize(phoenixForecast);
+            WriteLine(phoenixstring);
+
+            WeatherForecast w = JsonSerializer.Deserialize<WeatherForecast>(phoenixstring);
+            WriteLine(w.Summary);
+
+            ReadKey();
+        }
+    }
+}
+
---
+++ jsonreal/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using static System.Console;
+
+
+namespace jsonreal
+{
+    class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+        public int TemperatureCelsius { get; set; }
+        public string Summary { get; set; }
+    }
+}
